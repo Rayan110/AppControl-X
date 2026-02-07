192be8f54f3ea26a52322b1f4cf4ba00005ead03
risunCode <risunCode@users.noreply.github.com>
2026-02-07 11:47:41 +0700

    Fix Vite dev server import error
    - Add support for resolving paths in ESM mode by defining __dirname manually in vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
   plugins: [react()],
@@ -27,7 +27,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   server: {
