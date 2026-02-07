6b770385f037adb3994554015dd2c077d33d4806
risunCode <risunCode@users.noreply.github.com>
2026-02-07 17:12:26 +0700

    chore: bump version to 3.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.appcontrolx"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "3.0.0"
+        versionCode = 2
+        versionName = "3.1.0"
     }
 
     signingConfigs {
